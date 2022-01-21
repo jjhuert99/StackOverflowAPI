039a34d3325584538e58b51d4bf91224577fa8d1
jjhuert99 <jahuert99@gmail.com>
2022-01-21 17:40:18 -0600

    added dependency injection to retrofit objects and injected to viewModel

--- app/src/main/java/com/example/stackoverflowapi/network/StackOverflowEndPoints.kt
+++ app/src/main/java/com/example/stackoverflowapi/network/StackOverflowEndPoints.kt
@@ -4,7 +4,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface StackOverflowEndPoints {
-    @GET()
+    @GET("2.3/questions?fromdate=1640995200&todate=1642723200&order=desc&sort=activity&site=stackoverflow&filter=!216ygkw8.vGSXRH64k6U8")
     suspend fun getPosts():
             Response<Data>
 }
--- app/src/main/java/com/example/stackoverflowapi/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/stackoverflowapi/ui/home/HomeViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.stackoverflowapi.ui.home
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import com.example.stackoverflowapi.network.StackOverflowRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val app: Application,
+    private val StackOverflowRepo: StackOverflowRepo,
+    private val dispatcher: Dispatchers
+) : ViewModel() {
+
 }
