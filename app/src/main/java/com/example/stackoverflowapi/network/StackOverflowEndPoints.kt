fd6e26ad09076c6ed73fbf8482b769b55fa01d06
jjhuert99 <jahuert99@gmail.com>
2022-01-23 22:13:03 -0600

    added recycler view adapter, recyclerview binding, adapter functionality and altered url link for different results

--- app/src/main/java/com/example/stackoverflowapi/network/StackOverflowEndPoints.kt
+++ app/src/main/java/com/example/stackoverflowapi/network/StackOverflowEndPoints.kt
@@ -4,7 +4,8 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface StackOverflowEndPoints {
-    @GET("2.3/questions?fromdate=1640995200&todate=1642723200&order=desc&sort=activity&site=stackoverflow&filter=!216ygkw8.vGSXRH64k6U8")
+    //url yields post from july 2020 to january 2022, as of now is limited by page limititations
+    @GET("2.3/questions?pagesize=100&fromdate=1593561600&todate=1642896000&order=desc&sort=activity&site=stackoverflow&filter=!-KbmneoxUdFEsDppX_v9QlgN.p912M7vz")
     suspend fun getPosts():
             Response<Data>
 }
